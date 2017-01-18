ef713d473bf766e122451ebf982cb01a2241f37b
Igor Dorovskikh <i.dorovskikh@agilefusion.com>
2017-01-18 00:21:50 -0800

    updaing day1 code from live lecture

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -1,83 +1,59 @@
-/**
- * Created by idorovskikh on 12/12/16.
- */
-
 import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URL;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-
+/**
+ * Created by idorovskikh on 1/17/17.
+ */
 public class Login {
     AppiumDriver driver;
 
-    @BeforeSuite
-    public void setUp() throws MalformedURLException {
+    @BeforeSuite(alwaysRun = true)
+    public void setUp() throws IOException, InterruptedException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 6P");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(AndroidMobileCapabilityType.NO_SIGN, "true");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "us.moviemates");
+        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".Activities.SplashActivity");
+
 //        capabilities.setCapability(MobileCapabilityType.NO_RESET, "true");
         capabilities.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir") + "/app/app-debug.apk");
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 10);
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
 
-        this.driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         System.out.println(".......Starting Appium driver");
-
     }
 
     @AfterSuite
-    public void tearDown(){
+    public void tearDown() {
         System.out.println(".......Stopping Appium driver");
         driver.quit();
     }
 
     @Test
-    public void successfulFacebookLoginWithValidCredential() {
-        driver.findElement(By.id("btnLogin")).click();
+    public void successfulGoogleLoginWithValidCredential() {
+        driver.findElement(By.id("btnGoogleLogin")).click();
 
-        Set<String> contextHandles = driver.getContextHandles();
+        driver.findElement(By.id("android:id/text1")).click();
+        driver.findElement(By.id("android:id/button1")).click();
+        driver.findElementById("com.android.packageinstaller:id/permission_allow_button").click();
 
-        for (String s: contextHandles){
-            System.out.println("Context :" + s);
-            if (s.contains("WEBVIEW")){
-                System.out.println("Setting context to " + s);
-                driver.context(s);
-            }
-        }
+        Assert.assertTrue(driver.findElementById("btnHamburger").isDisplayed());
 
-        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("igordor@yahoo.com");
-        driver.findElement(By.xpath("//input[@name='pass']")).sendKeys("Expedia#1");
-
-        driver.hideKeyboard();
-
-        WebDriverWait driverWait = new WebDriverWait(driver, 10);
-        driverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@name='login']")));
-
-        driver.findElement(By.xpath("//button[@name='login']")).click();
-
-        driverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@name='__CONFIRM__']")));
-
-        driver.findElement(By.xpath("//button[@name='__CONFIRM__']")).click();
-        driver.context("NATIVE_APP");
-
-
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.android.packageinstaller:id/permission_allow_button")));
-        driver.findElement(By.id("com.android.packageinstaller:id/permission_allow_button")).click();
-
-        Assert.assertTrue(driver.findElementById("llHamburger").isDisplayed());
     }
-
 }
